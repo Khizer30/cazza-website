85ab36072d6a1864ad8b2dba59924ded55d5a124
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2026-02-24 17:56:31 +0500

    v0.00

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,28 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      "prettier/prettier": "warn",
+      "no-console": ["warn", { allow: ["warn", "error", "info"] }],
+      "prefer-const": "warn",
+      "no-trailing-spaces": "warn",
+      "no-var": "warn",
+      eqeqeq: ["warn", "always"],
+      curly: "warn"
+    }
+  },
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"])
 ]);
 
 export default eslintConfig;
